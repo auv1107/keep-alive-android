d5e92e539524b2d568f3a5c8497f8c68e3947d31
斯科特安 Scottian <auv1107@users.noreply.github.com>
2019-07-02 08:04:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## TabLayout
+
+Google推出了一系列实现 Material Design 效果的控件库，TabLayout 就是其中之一，用来实现指示器效果，本项目使用 TabLayout + VierPager + Fragment 实现了类Google Play排行榜模块的指示器，帮助开发者快速进行项目开发，将项目重点放在逻辑上。
+
+## Gradle
+
+    compile 'com.android.support:appcompat-v7:24.2.1'
+    compile 'com.android.support:design:24.2.1'
+
+## Preview
+
+ScreenShot文件夹为效果演示图（与项目代码无关）
+
+![](https://raw.githubusercontent.com/smartbetter/AndroidTabLayout/master/ScreenShot/screenshot0.gif)
+
+## License
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/antiless/vpnrank/MainActivity.java
@@ -0,0 +1,112 @@
+package com.antiless.vpnrank;
+
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.antiless.vpnrank.adapter.MyFragmentPagerAdapter;
+import com.antiless.vpnrank.fragment.AFragment;
+import com.antiless.vpnrank.fragment.BFragment;
+import com.antiless.vpnrank.fragment.CFragment;
+import com.antiless.vpnrank.fragment.DFragment;
+import com.antiless.vpnrank.fragment.EFragment;
+import com.antiless.vpnrank.fragment.FFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gc on 16/10/21.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private MyFragmentPagerAdapter myFragmentPagerAdapter;
+    private List<Fragment> listFragment;
+    private List<String> listTitle;
+
+    /**
+     * 此处的众多Fragment由开发者自定义替换
+     */
+    private AFragment aFragment;
+    private BFragment bFragment;
+    private CFragment cFragment;
+    private DFragment dFragment;
+    private EFragment eFragment;
+    private FFragment fFragment;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set Toolbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Show toolbar return arrow.
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        initView();
+    }
+
+    /**
+     * 初始化各控件
+     */
+    private void initView() {
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        // 初始化各fragment
+        aFragment = new AFragment();
+        bFragment = new BFragment();
+        cFragment = new CFragment();
+        dFragment = new DFragment();
+        eFragment = new EFragment();
+        fFragment = new FFragment();
+
+        // 将fragment装进列表中
+        listFragment = new ArrayList<>();
+        listFragment.add(aFragment);
+        listFragment.add(bFragment);
+        listFragment.add(cFragment);
+        listFragment.add(dFragment);
+        listFragment.add(eFragment);
+        listFragment.add(fFragment);
+
+        // 将名称加载tab名字列表，正常情况下，我们应该在values/arrays.xml中进行定义然后调用
+        listTitle = new ArrayList<>();
+        listTitle.add("热门免费");
+        listTitle.add("热门付费");
+        listTitle.add("创收最高");
+        listTitle.add("畅销付费新品");
+        listTitle.add("热门免费新品");
+        listTitle.add("上升最快");
+
+        // tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        // 设置TabLayout的模式
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        // tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        // 为TabLayout添加tab名称
+        tabLayout.addTab(tabLayout.newTab().setText(listTitle.get(0)));
+        tabLayout.addTab(tabLayout.newTab().setText(listTitle.get(1)));
+        tabLayout.addTab(tabLayout.newTab().setText(listTitle.get(2)));
+        tabLayout.addTab(tabLayout.newTab().setText(listTitle.get(3)));
+        tabLayout.addTab(tabLayout.newTab().setText(listTitle.get(4)));
+        tabLayout.addTab(tabLayout.newTab().setText(listTitle.get(5)));
+
+        myFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), listFragment, listTitle);
+
+        viewPager.setAdapter(myFragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
